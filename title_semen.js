41f8209e84f5f45317d4826393095bbb3e46f5be
Suzdalenko-Alexey <56520253+Suzdalenko-Alexey@users.noreply.github.com>
2020-03-08 15:01:31 +0100

    Update title_semen.js

--- title_semen.js
+++ title_semen.js
@@ -1,8 +1,10 @@
 {"name":   [
-    "пуратино"
+    "пуратино",
+    "MC ROBOT - Русь встала с колен, так резко что пиздец"
     ], 
 
  "uri" : [
-        "https://suzdalenko-alexey.github.io/s1/songs/a.mp3"
+        "https://suzdalenko-alexey.github.io/s1/songs/a.mp3",
+        "https://suzdalenko-alexey.github.io/s1/songs/MC ROBOT - Русь встала с колен, так резко что пиздец.mp3"
      ]         
                                           }
